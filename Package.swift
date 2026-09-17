9e8d4712acb5a51b0949af12b0ce3abbbb3ea271
stevewong9527 <97326298+stevewong9527@users.noreply.github.com>
2026-09-17 14:10:38 +0800

    chore: pin swift-sodium 0.11.0

--- Package.swift
+++ Package.swift
@@ -13,9 +13,18 @@ let package = Package(
             targets: ["VaultCore"]
         )
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/jedisct1/swift-sodium.git",
+            exact: "0.11.0"
+        )
+    ],
     targets: [
         .target(
-            name: "VaultCore"
+            name: "VaultCore",
+            dependencies: [
+                .product(name: "Sodium", package: "swift-sodium")
+            ]
         ),
         .testTarget(
             name: "VaultCoreTests",
