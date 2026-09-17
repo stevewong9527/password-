85ca4e3ca5b237b2bbedfc435a9ca478eb2738e7
stevewong9527 <97326298+stevewong9527@users.noreply.github.com>
2026-09-17 14:09:55 +0800

    feat: add libsodium Argon2id adapter

---
+++ Sources/VaultCore/SodiumArgon2IDKDF.swift
@@ -0,0 +1,47 @@
+#if canImport(Sodium)
+import Foundation
+import Sodium
+
+public struct SodiumArgon2IDKDF: PasswordKeyDeriver {
+    public let algorithmIdentifier = "argon2id13"
+    private let sodium: Sodium
+
+    public init() {
+        self.sodium = Sodium()
+    }
+
+    public func generateSalt(length: Int) throws -> Data {
+        guard length == sodium.pwHash.SaltBytes,
+              let bytes = sodium.randomBytes.buf(length: length) else {
+            throw RecoveryError.invalidSaltLength(length)
+        }
+        return Data(bytes)
+    }
+
+    public func deriveKey(
+        password: Data,
+        salt: Data,
+        outputLength: Int,
+        opsLimit: UInt64,
+        memLimit: UInt64
+    ) throws -> Data {
+        guard salt.count == sodium.pwHash.SaltBytes else {
+            throw RecoveryError.invalidSaltLength(salt.count)
+        }
+        guard opsLimit <= UInt64(Int.max), memLimit <= UInt64(Int.max) else {
+            throw RecoveryError.kdfParametersOutOfRange
+        }
+        guard let bytes = sodium.pwHash.hash(
+            outputLength: outputLength,
+            passwd: Array(password),
+            salt: Array(salt),
+            opsLimit: Int(opsLimit),
+            memLimit: Int(memLimit),
+            alg: .Argon2ID13
+        ) else {
+            throw RecoveryError.kdfFailed
+        }
+        return Data(bytes)
+    }
+}
+#endif
